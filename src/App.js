64c376730c1a73f565e425623fa3fffd67a75f1e
Taras Erechenko <ta300489@gmail.com>
2022-06-21 13:22:22 +0300

    custom hook usePosts

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MySelect from './components/UI/select/MySelect';
 import PostFilter from './components/PostFilter';
 import './styles/App.css';
 import MyModal from './components/UI/MyModal/MyModal';
+import {usePosts} from './hooks/usePost.js';
 
 function App() {
 
@@ -22,18 +23,8 @@ function App() {
     const [filter, setFilter] = React.useState({ sort: '', query: '' })
     const [modal,setModal]= React.useState(false)
    
-    const sortedPosts = React.useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;   
-    }, [filter.sort, posts])
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
     
-    const sortedAndSearchedPosts = React.useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query,sortedPosts])
-
-
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
