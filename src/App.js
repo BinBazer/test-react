9b802b3921df846c2ede6ff4825b3958c15e5b6d
Taras Erechenko <ta300489@gmail.com>
2022-06-21 13:55:25 +0300

    add PostService

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import './styles/App.css';
 import MyModal from './components/UI/MyModal/MyModal';
 import {usePosts} from './hooks/usePost.js';
 import axios from 'axios';
+import PostService from './API/PostService';
 
 function App() {
     const [posts, setPosts] = React.useState([]);
@@ -29,8 +30,8 @@ function App() {
     }
 
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(response.data)
+        const posts = await PostService.getAll();
+        setPosts(posts)
     }
 
     const removePost = (post) => {
