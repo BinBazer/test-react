2d7d00c26e1ec8a3027c57a0bd7e4acc908dda09
Taras Erechenko <ta300489@gmail.com>
2022-06-23 14:00:53 +0300

    add component AppRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { BrowserRouter as Router,Route, Routes, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './styles/App.css';
-import About from './pages/About';
-import Posts from './pages/Posts';
 import Navbar from './components/UI/Navbar/Navbar';
+import AppRouter from './components/AppRouter/AppRouter';
 
 
 function App() {
   return (  
     <Router>
-        <Navbar/>
-        <Routes>                
-            <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
-        </Routes>
+        <Navbar />
+        <AppRouter/>
     </Router>
   );
 }
--- src/utils/pages.js
+++ src/utils/pages.js
@@ -9,3 +9,4 @@ export const getPageArray = (totalPages) => {
     };
     return result;
 }
+
