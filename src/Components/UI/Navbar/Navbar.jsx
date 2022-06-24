c0c0ae5f342bdbfdbb86922a4b492a6d67a6b1d1
Taras Erechenko <ta300489@gmail.com>
2022-06-24 15:21:41 +0300

    exit account

--- src/Components/UI/Navbar/Navbar.jsx
+++ src/Components/UI/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { AuthContext } from './../../../context/index';
+import MyButton from './../button/MyButton';
+
+
 const Navbar = () => { 
+    const { isAuth, setIsAuth } = useContext(AuthContext)
+    
     return (
         <div className='navbar'>
+            <MyButton onClick={() => setIsAuth(false)}>Выйти</MyButton>
             <div className='navbar__links'>
                 <Link to='/about'>О сайте</Link>
                 <Link to='/posts'> Пости</Link>
